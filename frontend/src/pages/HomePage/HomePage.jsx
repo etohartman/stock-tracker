249bcd26f1e8bf6f11579b861905ed0b7b9aca18
etohartman <globalweb7777@gmail.com>
2025-06-19 21:28:51 -0700

    update css and homepage

--- frontend/src/pages/HomePage/HomePage.jsx
+++ frontend/src/pages/HomePage/HomePage.jsx
@@ -1,8 +1,8 @@
 export default function HomePage() {
-  return (
-    <>
-      <h1>Welcome To The Stock Tracker App</h1>
-      <h3>Please Sign Up, then you will be able to add and track your stocks.</h3>
-    </>
-  );
+return (
+  <div className="centered-content">
+    <h1>Welcome To The Stock Tracker App</h1>
+    <h3>Please Sign Up, then you will be able to add and track your stocks.</h3>
+  </div>
+);
 }
