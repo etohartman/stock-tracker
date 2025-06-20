b13408678931b1f26d4c12e9257714d911bfec83
etohartman <globalweb7777@gmail.com>
2025-06-19 21:16:48 -0700

    update homepage 3

--- frontend/src/pages/HomePage/HomePage.jsx
+++ frontend/src/pages/HomePage/HomePage.jsx
@@ -2,7 +2,7 @@ export default function HomePage() {
   return (
     <>
       <h1>Welcome To The Stock Tracker App</h1>
-      <h2>Please Sign Up, then you will be able to add and track your stocks.</h2>
+      <h3>Please Sign Up, then you will be able to add and track your stocks.</h3>
     </>
   );
 }
